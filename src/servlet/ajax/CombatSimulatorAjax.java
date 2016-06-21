b0715890988216133e0cd530a70f623f383b878c
Ryan <rvincent@ycp.edu>
2016-06-20 23:22:39 -0400

    Added an ajax servlet

---
+++ src/servlet/ajax/CombatSimulatorAjax.java
@@ -0,0 +1,33 @@
+package servlet.ajax;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CombatSimulatorAjax {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doRequest(req, resp);
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		doRequest(req, resp);
+	}
+
+	private void doRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// Use a controller to process the request
+		//GeneratorController controller = new GeneratorController();
+		//Double result = controller.start();
+		
+		// Send back a response
+		//resp.setContentType("text/plain");
+		//resp.getWriter().println(result.toString());
+	}
+}
+
