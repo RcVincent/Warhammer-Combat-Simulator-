cba1c94f2f1f1b29f25bd35adb4d495d3bb60391
Ryan <rvincent@ycp.edu>
2016-06-27 11:53:45 -0400

    Started work on add infantry and weapon serlets but need to work on the database first

---
+++ src/servlets/AddWeaponServlet.java
@@ -0,0 +1,12 @@
+package servlets;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddWeaponServlet {
+	private static final long seriealVersionUID = 1L;
+	
+	//need to build two databasemethods and two controller methods before anymore work can be done on these. 
+}
