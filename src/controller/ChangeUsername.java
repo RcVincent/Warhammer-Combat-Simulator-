2ca3cfea3510a0cc3ac80b76a2a864a6a5efd36b
Ryan <rvincent@ycp.edu>
2016-07-05 12:19:18 -0400

    change username controller

--- src/controller/ChangeUsername.java
+++ src/controller/ChangeUsername.java
@@ -1,5 +1,17 @@
 package controller;
+import DBpersist.DatabaseProvider;
+import DBpersist.DerbyDatabase;
+import DBpersist.IDatabase;
+
 
 public class ChangeUsername {
-
+	private IDatabase db = null;
+	public ChangeUsername(){
+		DatabaseProvider.setInstance(db);
+		db = DatabaseProvider.getInstance();
+	}
+	
+	public void changeUsername(String name, String newname, String password) {
+		db.changeUsername(name, newname, password);
+	}
 }
