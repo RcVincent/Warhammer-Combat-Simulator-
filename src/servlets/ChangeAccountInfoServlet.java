7db1e7ad5afc01607b77aa4bd9bdf0101cd96620
Ryan <rvincent@ycp.edu>
2016-07-05 20:45:59 -0400

    work on create account and change account info servlets

--- src/servlets/ChangeAccountInfoServlet.java
+++ src/servlets/ChangeAccountInfoServlet.java
@@ -1,5 +1,70 @@
 package servlets;
 
-public class ChangeAccountInfoServlet {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.User;
+import controller.DBMethodsController;
+
+public class ChangeAccountInfoServlet {
+private static final long serialVersionUID = 1L;
+	
+	private DBMethodsController c = new DBMethodsController(); 
+	
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String user = (String) req.getSession().getAttribute("username");
+		if (user == null) {
+			// user is not logged in, or the session expired
+			resp.sendRedirect(req.getContextPath() + "/Login");
+			return;
+		}
+		req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		// Decode form parameters and dispatch to controller
+		String errorMessage = null;
+		String username = null;
+		String newUsername = null;
+		String password = null;
+		
+		//get parameters from the jsp
+		username = req.getParameter("username");
+		newUsername = req.getParameter("newUsername");
+		password = req.getParameter("password");
+		
+		//Validate login credentials
+		ArrayList<User> user = null;
+		user = c.matchUser(username);
+		if(user.size()>0){
+			User u = user.get(0);
+			DBMethodsController controller = new DBMethodsController();
+			
+			//if user is authenticated, call change password
+			if(controller.authenticate(u, password)){
+				c.changeUsername(username, newUsername, password);
+				
+				//set session username to new username
+				req.getSession().setAttribute("username", newUsername);
+				errorMessage = "New username was successfully created";
+				req.setAttribute("errorMessage", errorMessage);
+				req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+			}
+			else{
+				
+				errorMessage = "Incorrect Username or Password";
+				req.setAttribute("errorMessage", errorMessage);
+				req.getRequestDispatcher("/_view/ChangeUsername.jsp").forward(req, resp);
+			}
+		}
+		//otherwise, print an error message
+	}
 }
--- src/servlets/CreateAccountServlet.java
+++ src/servlets/CreateAccountServlet.java
@@ -18,4 +18,30 @@ public class CreateAccountServlet {
 		throws ServletException, IOException {
 		req.getRequestDispatcher("/_view/CreateAccount.jsp").forward(req, resp);
 	}
+	
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String Firstname = null;
+		String Lastname = null;
+		String Username = null;
+		String Password = null;
+		String email = null;
+		String accountType = null;
+		
+		Firstname = req.getParameter("first name");
+		Lastname = req.getParameter("Lastname");
+		Username = req.getParameter("Username");
+		Password = req.getParameter("Password");
+		email = req.getParameter("email");
+		accountType = req.getParameter("accountType"); 
+		
+		//set session username
+		req.getSession().setAttribute("username", Username);
+				
+		
+		//Adds the user to the database
+		c.AddUser(Username, Password, email, accountType, Firstname, Lastname);
+		//Redirect to login page
+		req.getRequestDispatcher("/_view/Login.jsp").forward(req, resp);
+	}
 }
