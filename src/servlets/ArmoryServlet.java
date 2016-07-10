fd341b92ace75d1141bb63ef0471898f580323c0
Ryan <rvincent@ycp.edu>
2016-07-10 14:28:39 -0400

    minor work on servlets

--- src/servlets/ArmoryServlet.java
+++ src/servlets/ArmoryServlet.java
@@ -1,16 +1,14 @@
 package servlets;
+
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.DBMethodsController;
-
-public class AddWeaponServlet {
-	private static final long seriealVersionUID = 1L;
-	
-	//need to build two databasemethods and two controller methods before anymore work can be done on these. 
+public class ArmoryServlet {
 	private static final long serialVersionUID = 1L;
 	private DBMethodsController c = new DBMethodsController();
 	
