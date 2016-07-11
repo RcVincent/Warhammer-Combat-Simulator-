3a32d16c24712b01c06b7943ceae77e727ce9965
Ryan <rvincent@ycp.edu>
2016-07-10 20:44:31 -0400

    add weapon servlet

--- src/servlets/AddWeaponServlet.java
+++ src/servlets/AddWeaponServlet.java
@@ -7,20 +7,50 @@ import javax.servlet.http.HttpServletResponse;
 
 import controller.DBMethodsController;
 
+
 public class AddWeaponServlet {
 	private static final long seriealVersionUID = 1L;
 	
-	//need to build two databasemethods and two controller methods before anymore work can be done on these. 
+	//need to build two database methods and two controller methods before anymore work can be done on these. 
 	private static final long serialVersionUID = 1L;
 	private DBMethodsController c = new DBMethodsController();
 	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
+			String user = (String) req.getSession().getAttribute("username");
+			if (user == null) {
+				// user is not logged in, or the session expired
+				resp.sendRedirect(req.getContextPath() + "/Login");
+				return;
+			}
+			
+			req.getRequestDispatcher("/_view/AddWeapon.jsp").forward(req, resp);
 	}
 	
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		String message = null;
+		String weaponToRemove = null;
+		String weaponToAdd = null;
+		String NewItemPrice = req.getParameter("price");
+		String faction_name = (String)req.getSession().getAttribute("faction");
 		
+		weaponToRemove = req.getParameter("itemToRemove");
+		weaponToAdd = req.getParameter("item");
+		
+		if(weaponToAdd!=null){	
+			
+			c.AddWeapon(weaponToAdd, faction_name);
+			System.out.println("Success");
+			message = "Success";
+		}
+		if(weaponToRemove!=null){	
+			
+			c.RemoveWeapon(weaponToRemove);
+			System.out.println("Success");
+			message = "Success";
+		}
+		req.setAttribute("message", message);
+		req.getRequestDispatcher("/_view/AddWeapon.jsp").forward(req, resp);
 	}
 }
